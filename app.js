176a7746f10c67706c9edacf53777c89bbf8952c
Afreed Iqbal <afreediqbal@gmail.com>
2023-04-17 18:21:57 +0530

    changes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+const routes = require('/routes');
+
+app.use('/', routes); 
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
 
 app.listen(4000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 4000!');
 });
 
