9a82b4b7a4dd826999ec384e9c710c4c73fc26ff
Afreed Iqbal <afreediqbal@gmail.com>
2023-04-18 14:33:34 +0530

    testing data

---
+++ test/test.js
@@ -0,0 +1,36 @@
+const User = require('../src/model/user');
+const mongoose = require('mongoose');
+
+// Connect to database
+mongoose.connect('mongodb://localhost/myapp', { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to database');
+    
+    // Create a user
+    const user = new User({
+      email: 'test@example.com',
+      password: 'password',
+      role: 'user'
+    });
+    
+    // Save user to database
+    return user.save();
+  })
+  .then((savedUser) => {
+    console.log('User saved:', savedUser);
+    
+    // Find all users
+    return User.find({});
+  })
+  .then((users) => {
+    console.log('All users:', users);
+    
+    // Disconnect from database
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log('Disconnected from database');
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
